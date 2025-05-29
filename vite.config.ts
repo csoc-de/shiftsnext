f3aaf9872e3f3d4f5cc6d7d17583a26a9f9ba198
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2025-05-29 22:35:50 +0200

    build: simplify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { createAppConfig } from '@nextcloud/vite-config'
-import path from 'path'
 
 export default createAppConfig({
-	mainApp: path.join(__dirname, 'src', 'mainApp.ts'),
-	mainAdminSettings: path.join(__dirname, 'src', 'mainAdminSettings.ts'),
+	mainApp: 'src/mainApp.ts',
+	mainAdminSettings: 'src/mainAdminSettings.ts',
 }, {
 	inlineCSS: false,
 })
