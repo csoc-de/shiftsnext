988b5c5948598ab82a6f27cc8bdc3deda3673c0a
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2025-06-03 15:14:09 +0200

    feat: add RecoverableError class

--- src/models/error.ts
+++ src/models/error.ts
@@ -1,3 +1,12 @@
 export interface ErrorResponse {
 	error: string
 }
+
+export class RecoverableError extends Error {
+
+	constructor(message: string) {
+		super(message)
+		this.name = 'RecoverableError'
+	}
+
+}
