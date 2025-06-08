cb9b43eed7fdac1d029e671a1d4f7569d80d92eb
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2025-06-08 14:13:24 +0200

    fix: set app.config.idPrefix to APP_ID for consistent ID prefixing

--- src/mainApp.ts
+++ src/mainApp.ts
@@ -1,6 +1,7 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
+import { APP_ID } from './appId.ts'
 import router from './router/index.ts'
 import { initializeTailwindTheme } from './theme.ts'
 
@@ -11,6 +12,8 @@ import 'floating-vue/dist/style.css'
 initializeTailwindTheme()
 
 const app = createApp(App)
+
+app.config.idPrefix = APP_ID
 
 app.use(router)
 app.use(createPinia())
