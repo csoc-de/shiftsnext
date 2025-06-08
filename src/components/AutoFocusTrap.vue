5c6fd6d39eef5d1446f5e8ee02e364881b3e35d6
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2025-06-08 14:12:53 +0200

    fix: add dynamic id to AutoFocusTrap

--- src/components/AutoFocusTrap.vue
+++ src/components/AutoFocusTrap.vue
@@ -1,3 +1,9 @@
 <template>
-	<input class="float-left size-0 border-none p-0">
+	<input :id="id" class="float-left size-0 border-none p-0">
 </template>
+
+<script setup lang="ts">
+import { useId } from 'vue'
+
+const id = useId()
+</script>
