c8d076a2ccebe3862dd34ca31c4bd82509559497
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2025-06-08 16:04:39 +0200

    fix: set app.config.idPrefix to APP_ID for consistent ID prefixing

--- src/mainAdminSettings.ts
+++ src/mainAdminSettings.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import AdminSettingsView from './views/AdminSettingsView.vue'
+import { APP_ID } from './appId.ts'
 import { initializeTailwindTheme } from './theme.ts'
 
 import './date.ts' // Import for side effects: initializes localTimeZone
@@ -8,5 +9,7 @@ import '../css/style.scss'
 initializeTailwindTheme()
 
 const app = createApp(AdminSettingsView)
+
+app.config.idPrefix = APP_ID
 
 app.mount('#admin-settings')
