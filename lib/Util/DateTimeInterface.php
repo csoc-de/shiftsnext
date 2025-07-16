3a5a24a91c2162df7d3897912ed9dc12f22d5469
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2025-07-16 15:19:30 +0200

    fix(DateTimeInterface): remove invalid final keyword from constants

--- lib/Util/DateTimeInterface.php
+++ lib/Util/DateTimeInterface.php
@@ -9,27 +9,27 @@ interface DateTimeInterface extends \DateTimeInterface {
 	 * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#date_time_string_format ECMAScript Date Time String}
 	 * (example: 1970-01-01T00:00:00.000Z)
 	 */
-	final public const string ECMA_DATE_TIME = 'Y-m-d\TH:i:s.vp';
+	public const string ECMA_DATE_TIME = 'Y-m-d\TH:i:s.vp';
 
 	/**
 	 * {@link https://www.rfc-editor.org/rfc/rfc9557.html RFC 9557}
 	 * without calendar (example: 1970-01-01T00:00:00.000+00:00[UTC])
 	 */
-	final public const string RFC9557_NC = 'Y-m-d\TH:i:s.vP\[e\]';
+	public const string RFC9557_NC = 'Y-m-d\TH:i:s.vP\[e\]';
 
 	/**
 	 * Same as {@see OCA\ShiftsNext\Util\DateTimeInterface::RFC9557_NC} but
 	 * without fractional second
 	 */
-	final public const string RFC9557_NC_NF = 'Y-m-d\TH:i:sP\[e\]';
+	public const string RFC9557_NC_NF = 'Y-m-d\TH:i:sP\[e\]';
 
 	/**
 	 * A plain date (example: 1970-01-01)
 	 */
 
-	final public const string PLAIN_DATE = 'Y-m-d';
+	public const string PLAIN_DATE = 'Y-m-d';
 	/**
 	 * iCal plain date (example: 19700101)
 	 */
-	final public const string I_CAL_PLAIN_DATE = 'Ymd';
+	public const string I_CAL_PLAIN_DATE = 'Ymd';
 }
