8d10f0357f6fc8eb7d1e26782e3bbff0a47e9755
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2025-11-25 09:10:50 +0100

    refactor(ShiftTypePill): bind "assign shift" click handler to dedicated button instead of Pill component

--- src/components/ShiftTypePill.vue
+++ src/components/ShiftTypePill.vue
@@ -4,35 +4,54 @@
 			backgroundColor: shiftTypeWrapper.shiftType.color,
 			color: contrastColor,
 		}"
-		class="flex items-center justify-between gap-2 rounded-nc-container p-2"
+		class="flex items-center justify-between gap-1 rounded-nc-container p-2"
 		:class="{
 			underline: isSelected,
-			'pointer-events-none': multiStepAction.type,
-		}"
-		@click.stop="_isShiftAdmin && onClick()">
-		<div class="truncate leading-[1.1]">
+		}">
+		<div
+			class="truncate leading-[1.1]">
 			{{ shiftTypeWrapper.shiftType.group.display_name }}<br>
 			{{ shiftTypeWrapper.shiftType.name }}
 		</div>
-		<NcCounterBubble
-			v-if="shiftTypeWrapper.amount > 1"
-			:style="{
-				backgroundColor: contrastColor,
-				color: doubledContrastColor,
-			}"
-			:count="shiftTypeWrapper.amount" />
+		<div
+			v-if="_isShiftAdmin"
+			class="flex gap-1 items-center">
+			<NcCounterBubble
+				v-if="shiftTypeWrapper.amount > 1"
+				:style="{
+					backgroundColor: contrastColor,
+					color: doubledContrastColor,
+				}"
+				:count="shiftTypeWrapper.amount" />
+			<NcButton
+				:disabled="disabled"
+				:aria-label="t(APP_ID, 'Assign shift')"
+				variant="tertiary-no-background"
+				class="border"
+				:style="{ color: contrastColor, borderColor: contrastColor }"
+				@click.stop="onAssignButtonClick">
+				<template #icon>
+					<ArrowAll :size="24" />
+				</template>
+			</NcButton>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { t } from '@nextcloud/l10n'
 import { computed, inject } from 'vue'
+import NcButton from '@nextcloud/vue/components/NcButton'
 import NcCounterBubble from '@nextcloud/vue/components/NcCounterBubble'
+// @ts-expect-error package has no types
+import ArrowAll from 'vue-material-design-icons/ArrowAll.vue'
 import {
 	type ShiftTypeWrapper,
 
 	multiStepActionIK,
 	setMultiStepActionIK,
 } from '../models/shiftsTable.ts'
+import { APP_ID } from '../utils/appId.ts'
 import { getContrastColor } from '../utils/color.ts'
 import { isShiftAdmin } from '../utils/groupShiftAdmin.ts'
 
@@ -56,7 +75,9 @@ const doubledContrastColor = computed(() => getContrastColor(contrastColor.value
 /**
  * Handle click event
  */
-function onClick() {
+function onAssignButtonClick() {
 	setTableAction({ type: 'creation', columnIndex, shiftTypeWrapper })
 }
+
+const disabled = computed(() => Boolean(multiStepAction.value.type))
 </script>
