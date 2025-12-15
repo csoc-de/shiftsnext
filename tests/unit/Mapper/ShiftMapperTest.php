de34fff5695bb967633d2eb0bb26887920f4ff5d
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2025-12-15 17:02:51 +0100

    test(ShiftMapperTest): align time zone format of test data with format used during app runtime

--- tests/unit/Mapper/ShiftMapperTest.php
+++ tests/unit/Mapper/ShiftMapperTest.php
@@ -27,23 +27,23 @@ class ShiftMapperTest extends TestCase {
 		[
 			'userId' => 'user1',
 			'shiftTypeId' => 1, // Black Team Early shift
-			'start' => '2025-01-01T07:00:00.000Z',
-			'end' => '2025-01-01T11:30:00.000Z',
+			'start' => '2025-01-01T07:00:00+00:00[UTC]',
+			'end' => '2025-01-01T11:30:00+00:00[UTC]',
 		], [
 			'userId' => 'user2',
 			'shiftTypeId' => 2, // Black Team Late shift
-			'start' => '2025-01-01T11:30:00.000Z',
-			'end' => '2025-01-01T16:00:00.000Z',
+			'start' => '2025-01-01T11:30:00+00:00[UTC]',
+			'end' => '2025-01-01T16:00:00+00:00[UTC]',
 		], [
 			'userId' => 'user3',
 			'shiftTypeId' => 3, // Blue Team Early shift
-			'start' => '2025-01-01T05:00:00.000Z',
-			'end' => '2025-01-01T13:00:00.000Z',
+			'start' => '2025-01-01T05:00:00+00:00[UTC]',
+			'end' => '2025-01-01T13:00:00+00:00[UTC]',
 		], [
 			'userId' => 'user4',
 			'shiftTypeId' => 4, // Blue Team Late shift
-			'start' => '2025-01-01T13:00:00.000Z',
-			'end' => '2025-01-01T21:00:00.000Z',
+			'start' => '2025-01-01T13:00:00+00:00[UTC]',
+			'end' => '2025-01-01T21:00:00+00:00[UTC]',
 		], [
 			'userId' => 'user2',
 			'shiftTypeId' => 6, // Black Team Office duty
