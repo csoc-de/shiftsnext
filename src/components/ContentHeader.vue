4a8ea00a5bca06b344e52daa2baf7c8dfc9eac5f
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2026-01-20 13:27:14 +0100

    fix(ContentHeader): overwrite style of headings

--- src/components/ContentHeader.vue
+++ src/components/ContentHeader.vue
@@ -2,7 +2,7 @@
 	<div
 		class="sticky top-0 z-[20] flex w-full flex-wrap items-center justify-start gap-[calc(2*var(--app-navigation-padding))] py-[var(--app-navigation-padding)] pe-[var(--app-navigation-padding)] ps-[calc(var(--default-clickable-area)+2*var(--app-navigation-padding))]"
 		role="toolbar">
-		<h2 class="max-w-64 truncate m-0 leading-[var(--default-clickable-area)]">
+		<h2 class="max-w-64 truncate">
 			{{ title }}
 		</h2>
 
@@ -26,3 +26,12 @@ defineProps<{
 	loading: boolean
 }>()
 </script>
+
+<style scoped lang="scss">
+:deep() {
+	h1, h2, h3, h4, h5, h6 {
+		line-height: initial;
+		margin: 0;
+	}
+}
+</style>
