4c17bda6d72d0fd4350468647bc45a43d005837f
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2026-01-21 17:49:41 +0100

    feat: include appVersion header in api requests

--- src/mainAdminSettings.ts
+++ src/mainAdminSettings.ts
@@ -1,12 +1,15 @@
+import axios from '@nextcloud/axios'
 import { createApp } from 'vue'
 import AdminSettingsView from './views/AdminSettingsView.vue'
-import { APP_ID } from './utils/appId.ts'
+import { APP_ID, APP_VERSION } from './utils/appId.ts'
 import { initializeTailwindTheme } from './utils/theme.ts'
 
 import './utils/date.ts' // Import for side effects: initializes userTimeZone
 import '../css/style.scss'
 
 initializeTailwindTheme()
+
+axios.defaults.headers[`X-${APP_ID}-appVersion`] = APP_VERSION
 
 const app = createApp(AdminSettingsView)
 
--- src/mainApp.ts
+++ src/mainApp.ts
@@ -1,14 +1,17 @@
+import axios from '@nextcloud/axios'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.ts'
-import { APP_ID } from './utils/appId.ts'
+import { APP_ID, APP_VERSION } from './utils/appId.ts'
 import { initializeTailwindTheme } from './utils/theme.ts'
 
 import './utils/date.ts' // Import for side effects: initializes userTimeZone
 import '../css/style.scss'
 
 initializeTailwindTheme()
+
+axios.defaults.headers[`X-${APP_ID}-appVersion`] = APP_VERSION
 
 const app = createApp(App)
 
