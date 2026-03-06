a0cc97fc86c3c1fcc2e3144a67979a43c98eff94
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2026-03-06 16:05:56 +0100

    feat(groupShiftAdmin): add getShiftAdmins function

--- src/utils/groupShiftAdmin.ts
+++ src/utils/groupShiftAdmin.ts
@@ -1,8 +1,12 @@
 import type { Group } from '../models/group.ts'
+import type { GroupShiftAdminRelationsByGroup } from '../models/groupShiftAdminRelation.ts'
+import type { User } from '../models/user.ts'
 
-import { getInitialShiftAdminGroups } from './initialState.ts'
+import { getInitialGroupShiftAdminRelationsByGroup, getInitialShiftAdminGroups } from './initialState.ts'
+import { compare } from './sort.ts'
 
 let shiftAdminGroups: Group[]
+let groupedRelations: GroupShiftAdminRelationsByGroup[]
 
 /**
  * Checks if the logged-in user is a group shift admin of `groupId`
@@ -23,4 +27,24 @@ export function isShiftAdminAll(groupIds: string[]): boolean {
 	shiftAdminGroups ??= getInitialShiftAdminGroups()
 	return groupIds
 		.every((groupId) => shiftAdminGroups.some(({ id }) => id === groupId))
+}
+
+/**
+ * Returns the shift admins for `groupIds` sorted by their display name
+ *
+ * @param groupIds The group IDs to get the shift admins for
+ */
+export function getShiftAdmins(groupIds: string[]): User[] {
+	groupedRelations ??= getInitialGroupShiftAdminRelationsByGroup()
+	const shiftAdmins = new Map<string, User>()
+	for (const groupId of groupIds) {
+		const _shiftAdmins = groupedRelations
+			.find(({ group }) => group.id === groupId)?.users ?? []
+		for (const _shiftAdmin of _shiftAdmins) {
+			shiftAdmins.set(_shiftAdmin.id, _shiftAdmin)
+		}
+	}
+	return Array
+		.from(shiftAdmins.values())
+		.sort((a, b) => compare(a.display_name, b.display_name))
 }
